c2d9f748f165857770dfca0fcc584a380791956e
Jesper B. Rosenkilde <jbr@humppa.dk>
2023-02-21 12:39:11 +0100

    Cleanup

--- zinit-annex-noalias/zinit-annex-noalias.plugin.zsh
+++ zinit-annex-noalias/zinit-annex-noalias.plugin.zsh
@@ -5,16 +5,12 @@
 
 zinit-annex-noalias-help() { : }
 
-zinit_annex_tmpdir="${ZINIT[HOME_DIR]}/tmp"
-mkdir -p "${zinit_annex_tmpdir}"
-
-
 zinit-annex-noalias() {
   [[ ${+ICE[noalias]} = 0 ]] && return 0
 
   [[ "$1" = plugin ]] && \
-    local id="${2}--${3}" hook="$6" || \
-    local id="$2" hook="$5" # type: snippet
+    local hook="$6" || \
+    local hook="$5" # type: snippet
 
   if [[ $hook = atinit ]]; then
     alias () { : }
